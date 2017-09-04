2e6e50eb36234fa90e7832558ef29cef4867a0c4
JreeyPaladin <184252426@qq.com>
2017-09-04 21:32:27 +0800

    添加序列化对象

---
+++ EasyOA/OAEntities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OAEntities
+{
+    public class BaseEntity
+    {
+        public string Action { get; set; }
+        public object Data { get; set; }
+    }
+}
